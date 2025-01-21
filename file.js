548ce5e2cfc49ab67945980eb012184a3032b038
rafiahabib <rafiahabib46@gmail.com>
2025-01-21 17:34:05 +0500

    Update file.js

--- file.js
+++ file.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Simple JavaScript Example</title>
-</head>
-<body>
-    <h1 id="greeting"></h1>
 
-    <script>
-        // Display the message on the webpage
-        document.getElementById("greeting").innerText = "Hello, World!";
-    </script>
-</body>
-</html>
